d57efa2a3a287674095f009e5b23030f72f600fe
VictorC1707 <victor.canchis7@gmail.com>
2026-05-29 17:34:40 -0400

    Tema 2

---
+++ Tema 2/Python/vectores.py
@@ -0,0 +1,9 @@
+import math
+import random
+import time
+
+n = 2_000_000           # Tamaño de los vectores (ajústalo según tu máquina)
+SEMILLA = 123456789     # Semilla fija para reproducibilidad entre lenguajes
+random.seed(SEMILLA)
+
+print(random.seed(SEMILLA))
