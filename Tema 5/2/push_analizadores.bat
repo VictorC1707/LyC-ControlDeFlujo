285508da2f769ddcef017aeeb0a466d840b75b02
Diosdado <diosdadomunoz700@gmail.com>
2026-07-25 21:35:51 -0400

    Eliminar script push_analizadores.bat del repositorio

--- Tema 5/2/push_analizadores.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Iniciando la subida a GitHub...
-"C:\Users\alexm\OneDrive\Documents\Redes\PortableGit\cmd\git.exe" push origin main
-echo.
-echo Proceso finalizado. Puedes cerrar esta ventana.
-pause
